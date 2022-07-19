9336f2e9f71cfda4763a34ef69a382c947a5b169
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-07-19 12:43:33 -0300

    feat(SingIn): import components and create states

--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {VStack, Heading, Icon, useTheme} from 'native-base';
 import Logo from '../assets/logo_primary.svg';
 import { Envelope, Key } from 'phosphor-react-native';
 import { Input } from '../components/input';
+import { Button } from '../components/Button';
 
 export function SingIn() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
     const {colors} = useTheme()
+
+    const handleSingIn = () => {
+        
+    }
 
     return (
         <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
@@ -19,13 +27,17 @@ export function SingIn() {
                 placeholder='E-mail'
                 mb={4}
                 InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+                onChangeText={setName}
             />
 
             <Input 
                 placeholder='Senha'
                 InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+                secureTextEntry
+                onChangeText={setPassword}
             />
-
+            
+            <Button title='Logar' w='full' onPress={handleSingIn} />
         </VStack>
     )
 }
